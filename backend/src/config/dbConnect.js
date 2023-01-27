e59b7f4df945e1ba1ddfd4deb7ce32d44a29bdad
Govind Singh <govindkmr141@gmail.com>
2023-01-27 16:34:17 +0530

    finish

--- backend/src/config/dbConnect.js
+++ backend/src/config/dbConnect.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connection = () => {
-  return mongoose.connect("mongodb://localhost:27017/fundu");
+  return mongoose.connect(
+    "mongodb+srv://govind1:123@cluster0.3cdklpi.mongodb.net/fundu?retryWrites=true&w=majority"
+  );
 };
 
 module.exports = connection;
